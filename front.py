0aaadfe489e9d097f813ec5ed63dc906be395666
Ayush singh <163518600+ayushsingh991399@users.noreply.github.com>
2025-07-14 17:26:33 +0530

    Update front.py

--- front.py
+++ front.py
@@ -9,7 +9,7 @@ from PIL import Image
 st.set_page_config(page_title="Customer Churn Prediction", layout="wide")
 
 # Optional banner image (local or hosted)
-st.image("https://thumbs.dreamstime.com/b/customer-churn-rate-concept-client-attrition-d-illustration-customers-analysis-162267469.jpg", use_container_width=True)
+st.image("https://thumbs.dreamstime.com/b/customer-churn-rate-concept-client-attrition-d-illustration-customers-analysis-162267469.jpg", width=800)
 
 # App Header
 st.title("✨ Customer Churn Prediction")
